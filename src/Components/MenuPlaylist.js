868b26d1322b7eaf5da26a6b5575e530529ce5e7
Sagar <sagarpathak8826@gmail.com>
2022-09-10 20:08:44 +0530

    Initial commit

---
+++ src/Components/MenuPlaylist.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {FaPlus} from "react-icons/fa";
+import {BsMusicNoteList, BsTrash} from "react-icons/bs";
+import {PlayList} from './PlayList';
+
+
+function MenuPlaylist() {
+  return ( <div className='playListContainer'>
+    <div className="nameContainer">
+        <p> PlayList</p>
+        <i>
+            <FaPlus />
+        </i>
+    </div>
+    <div className="playListScroll"> 
+    {
+        PlayList && PlayList.map((list) =>(
+    
+       <div className="playlist" key={list.id}>
+        <i className="list">
+            <BsMusicNoteList />
+        </i>
+        <p>
+            {list.name}
+        </p>
+        <i className='trash'>
+            <BsTrash />
+        </i>
+     </div>
+     ))}
+    </div>
+  </div>
+  );
+}
+
+export  {MenuPlaylist}
---
+++ src/Components/TrackList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {BsFillVolumeUpFill, BsMusicNoteList} from "react-icons/bs";
+import {FaDesktop} from "react-icons/fa";
+import Track from "../img/track.png";
+
+function TrackList() {
+  return <div className="trackList">
+    <div className="top">
+        <img src ={Track} alt=""/>
+        <p className='trackname'>
+            Sample Name <span className="trackSpan">Artist</span>
+        </p>
+    </div>
+    <div className="bottom">
+        <i>
+            <BsFillVolumeUpFill />
+        </i>
+        <input type="range" />
+        <i>
+            <FaDesktop />
+        </i>
+
+    </div>
+  </div>;
+}
+
+export  {TrackList}
